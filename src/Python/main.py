6887b9690fb551394ecfef534d1bb92268781d87
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-04-24 16:12:53 +0430

    fixed som bugs

--- src/Python/main.py
+++ src/Python/main.py
@@ -2,7 +2,7 @@ from math import sqrt
 
 D = "Which side are you looking for? "
 
-# a**2 = b**2 + c**2
+# c**2 = b**2 + a**2
 
 
 while type(D) == str:
@@ -14,29 +14,29 @@ while type(D) == str:
 	if x.lower() == "a":
 		f = input("give me (b) and (c): ")
 		b, c = f.split(' ')
-		a = sqrt((int(b)**2) + (int(c)**2))
+		a = sqrt((int(c)**2) - (int(b)**2))
 		print("\nSide (a) is equal to %.2f" % (a))
 
 	#b
 	elif x.lower() == "b":
 		f = input("give me (a) and (c): ")
 		a, c = f.split(' ')
-		b = sqrt((int(a)**2) + (int(c)**2))
+		b = sqrt((int(c)**2) - (int(a)**2))
 		print("\nSide (b) is equal to %.2f" % (b))
 
 	#c
 	elif x.lower() == "c":
 		f = input("give me (a) and (b): ")
 		a, b = f.split(' ')
-		c = sqrt((int(a)**2) - (int(b)**2))
+		c = sqrt((int(a)**2) + (int(b)**2))
 		print("\nSide (b) is equal to %.2f" % (c))
 
 	elif x.lower() == "exit":
 
 		print("=======================================================================")
 
-		s = input("Do you want to calculate again?[y, n]: ")
+		s = input("Do you want to exit?[y, n]: ")
 		if s.lower() == "y":
-			continue
-		else:
 			break
+		elif s.lower() == "n":
+			continue
